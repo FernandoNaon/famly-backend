71f58fed30f72c0dc9cc71e49e32e7dcd0303bcf
Fernando Naon <fernandonaon@gmail.com>
2024-04-20 16:25:08 +0200

    update sql query

--- app.js
+++ app.js
@@ -54,6 +54,41 @@ app.get("/messages-by-user/:userId", (req, res) => {
 
 // // [2]- All messages in conversation where users 1 and 3 are participating (other users could also be participating)
 
+// app.get("/messages-by-users/:userId1/:userId2", (req, res) => {
+//   const userId1 = req.params.userId1;
+//   const userId2 = req.params.userId2;
+
+//   if (isNaN(userId1) || isNaN(userId2)) {
+//     res.status(400).json({ error: "Invalid userId" });
+//     return;
+//   }
+
+//   connection.query(
+//     `
+//     SELECT m.id, m.userId, m.conversationId, m.txt
+//     FROM message m
+//     INNER JOIN conversation c ON m.conversationId = c.id
+//     WHERE EXISTS (
+//         SELECT 1 FROM conversation c1
+//         WHERE c1.id = c.id AND c1.userId = ?
+//     )
+//     AND EXISTS (
+//         SELECT 1 FROM conversation c2
+//         WHERE c2.id = c.id AND c2.userId = ?
+//     );
+//     `,
+//     [userId1, userId2],
+//     (err, results) => {
+//       if (err) {
+//         console.error("Error fetching data:", err);
+//         res.status(500).json({ error: "Error fetching data" });
+//         return;
+//       }
+//       res.json(results);
+//     }
+//   );
+// });
+
 app.get("/messages-by-users/:userId1/:userId2", (req, res) => {
   const userId1 = req.params.userId1;
   const userId2 = req.params.userId2;
@@ -65,16 +100,11 @@ app.get("/messages-by-users/:userId1/:userId2", (req, res) => {
 
   connection.query(
     `
-    SELECT f.id, f.userId, f.conversationId, f.txt
-    FROM message f
-    INNER JOIN conversation s ON f.conversationId = s.id
-    WHERE EXISTS (
-        SELECT 1 FROM conversation c1
-        WHERE c1.id = s.id AND c1.userId = ?
-    )
-    AND EXISTS (
-        SELECT 1 FROM conversation c2
-        WHERE c2.id = s.id AND c2.userId = ?
+    SELECT * FROM message
+    WHERE conversationId IN (
+        SELECT DISTINCT conversationId
+        FROM message
+        WHERE userId = ? OR userId = ?
     );
     `,
     [userId1, userId2],
